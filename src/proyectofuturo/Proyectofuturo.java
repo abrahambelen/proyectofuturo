b2603ca858cc5a7a2769eb21ebe2325794aa0411
Abraham <abraham.belen.duran@gmail.com>
2022-11-17 17:53:37 +0100

    Primer commit

---
+++ src/proyectofuturo/Proyectofuturo.java
@@ -0,0 +1,27 @@
+package proyectofuturo;
+
+public class Proyectofuturo {
+    
+    static String veredicto(String valor1, String valor2) {
+
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) > Integer.valueOf(valor2)) {
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+
+    }
+
+    public static void main(String[] args) {
+        String program;
+       
+        System.out.println("Estudiante " + args[0]);
+        
+        program =  veredicto( args[1] , args[2]);
+        System.out.println(program);
+    
+    }
+    
+}
